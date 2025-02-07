5945f7b876dce21ea7c84978b69db51064cc28d7
Varun Posimsetty <VarunSrinivas.Posimsetty@infineon.com>
2025-02-07 12:28:14 +0100

    working multiplier

--- unitMultiplier/src/fp_mul.vhd
+++ unitMultiplier/src/fp_mul.vhd
@@ -7,7 +7,7 @@ entity fp_mul is
     generic(
         EXP_WIDTH  : integer := 11; -- EXPONENT WIDTH (Float := 8 , Double := 11 )
         MANT_WIDTH : integer := 52; -- MANTISSA WIDTH (Float := 23, Double := 52 )
-        BAIS       : integer := 1023 -- BIAS VALUE (Float := 127 , Double := 1023)
+        BIAS       : integer := 1023 -- BIAS VALUE (Float := 127 , Double := 1023)
     );
     port(
         i_clk_100MHz : in std_ulogic;
@@ -21,6 +21,7 @@ end entity fp_mul;
 architecture RTL of fp_mul is 
     -- CONSTANTS 
     constant all_one :  std_ulogic_vector(MANT_WIDTH downto 0)  := (others => '1');
+    constant bias_unsigned : unsigned(EXP_WIDTH-1 downto 0) := to_unsigned(BIAS,EXP_WIDTH);
     -- CAPTURE 
     signal sign_1      : std_ulogic := '0';
     signal sign_2      : std_ulogic := '0';
@@ -44,7 +45,6 @@ architecture RTL of fp_mul is
     signal significand_product : std_ulogic_vector(MANT_WIDTH downto 0) := (others => '0');
     signal norm_significand : std_ulogic_vector(MANT_WIDTH-1 downto 0) := (others => '0');
     signal norm_exponent : std_ulogic_vector(EXP_WIDTH-1 downto 0) := (others => '0');
-    signal temp_significand_product : std_ulogic_vector(MANT_WIDTH downto 0);
     signal result : std_ulogic_vector(1+EXP_WIDTH+MANT_WIDTH-1 downto 0) := (others => '0');
 
     begin 
@@ -68,41 +68,41 @@ architecture RTL of fp_mul is
                 end if;
         end process proc_capture;
 
-        -- DENORMALIZATION 
+        -- DENORMALIZATION
         proc_denorm : process(i_clk_100MHz,i_nrst_async) is 
-        begin 
-            if(i_nrst_async = '0') then 
-                unnorm_exponent_integer <= 0;
-                unnorm_exponent <= (others => '0');
-                implicit_1 <= '0';
-                implicit_2 <= '0';
-                significand_1 <= (others => '0');
-                significand_2 <= (others => '0');
-                output_sign <= '0';
-            elsif(rising_edge(i_clk_100MHz)) then
-                unnorm_exponent_integer <= to_integer(unsigned(exponent_1)) + to_integer(unsigned(exponent_2)) - integer(BAIS);
-                if (unnorm_exponent_integer <= 0) then 
+            begin 
+                if(i_nrst_async = '0') then
+                    unnorm_exponent_integer <= 0;
                     unnorm_exponent <= (others => '0');
-                elsif (unnorm_exponent_integer > 2*BAIS) then
-                    unnorm_exponent <= (others => '1'); -- (others => '1') equals 2*BAIS + 1
-                else 
-                    unnorm_exponent <= std_ulogic_vector(std_logic_vector(to_unsigned(unnorm_exponent_integer,EXP_WIDTH)));
-                end if;
-                if(to_integer(unsigned(exponent_1)) = 0) then 
                     implicit_1 <= '0';
-                else 
-                    implicit_1 <= '1';
-                end if;
-                if(to_integer(unsigned(exponent_2)) = 0) then 
                     implicit_2 <= '0';
-                else 
-                    implicit_2 <= '1';
+                    significand_1 <= (others => '0');
+                    significand_2 <= (others => '0');
+                    output_sign <= '0';
+                elsif(rising_edge(i_clk_100MHz)) then
+                    unnorm_exponent_integer <= to_integer(unsigned(exponent_1)) + to_integer(unsigned(exponent_2)) - BIAS;
+                    if (unnorm_exponent_integer <= 0) then 
+                        unnorm_exponent <= (others => '0');
+                    elsif (unnorm_exponent_integer > 2*BIAS) then
+                        unnorm_exponent <= (others => '1'); -- (others => '1') equals 2*BAIS + 1
+                    else 
+                        unnorm_exponent <= std_ulogic_vector(unsigned(exponent_1) + unsigned(exponent_2) - bias_unsigned);
+                    end if;
+                    if(to_integer(unsigned(exponent_1)) = 0) then 
+                        implicit_1 <= '0';
+                    else 
+                        implicit_1 <= '1';
+                    end if;
+                    if(to_integer(unsigned(exponent_2)) = 0) then 
+                        implicit_2 <= '0';
+                    else 
+                        implicit_2 <= '1';
+                    end if;
+                    significand_1 <= implicit_1 & mantissa_1;
+                    significand_2 <= implicit_2 & mantissa_2;
+                    output_sign <= sign_1 xor sign_2;
                 end if;
-                significand_1 <= implicit_1 & mantissa_1;
-                significand_2 <= implicit_2 & mantissa_2;
-                output_sign <= sign_1 xor sign_2;
-            end if;
-        end process proc_denorm;
+        end process proc_denorm;  
 
         -- SIGNIFICAND MULTIPLICATION
         proc_sig_mul : process(i_clk_100MHz,i_nrst_async) is 
@@ -117,13 +117,15 @@ architecture RTL of fp_mul is
                     product <= std_ulogic_vector(unsigned(significand_1) * unsigned(significand_2));
                 end if;
         end process proc_sig_mul;
-
+        
         proc_norm : process(i_clk_100MHz,i_nrst_async) is
             variable l : std_ulogic := '0';
             variable g : std_ulogic := '0';
             variable s : std_ulogic := '0';
             variable r : std_ulogic := '0';
-            variable k : integer    :=  0 ;
+            variable k : unsigned(MANT_WIDTH downto 0) :=  (others => '0') ;
+            variable temp_significand_product : std_ulogic_vector(MANT_WIDTH downto 0);
+            variable norm_exponent_var : std_ulogic_vector(EXP_WIDTH-1 downto 0) := (others => '0');
             begin 
                 if(i_nrst_async = '0') then 
                     l := '0';
@@ -133,7 +135,8 @@ architecture RTL of fp_mul is
                     significand_product <= (others => '0');
                     norm_exponent <= (others => '0');
                     norm_significand <= (others => '0');
-                    temp_significand_product <= (others => '0');
+                    temp_significand_product := (others => '0');
+                    norm_exponent_var := (others => '0');
                     result <= (others => '0');
                 elsif(rising_edge(i_clk_100MHz)) then
                     significand_product <= product(2*(MANT_WIDTH+1)-1 downto MANT_WIDTH + 1);
@@ -145,20 +148,21 @@ architecture RTL of fp_mul is
                     end loop;
                     r := (g and (l or s));
                     if (r = '0') then 
-                        k := 0; 
-                    elsif (r = '1') then 
-                        k := 1;
-                    end if;
-                    if((significand_product = all_one ) and (r = '1')) then 
-                        norm_significand <= (others => '0');
-                        norm_exponent <= std_ulogic_vector(to_unsigned(to_integer(unsigned(unnorm_exponent_stage)) + 1, EXP_WIDTH));
+                        k(0) := '0';
                     else 
-                        temp_significand_product <= std_ulogic_vector(to_unsigned(to_integer(unsigned(significand_product)) + k, MANT_WIDTH+1));
-                        norm_significand <= temp_significand_product(temp_significand_product'high - 1 downto 0);
+                        k(1) := '1';
+                    end if;
+                    if((significand_product = all_one) and (r = '1')) then 
+                        norm_significand <= (others => '0');
+                        norm_exponent_var := std_ulogic_vector(unsigned(unnorm_exponent_stage) + k);
+                        norm_exponent <= norm_exponent_var;
+                    else
+                        temp_significand_product := std_ulogic_vector(unsigned(significand_product) + k);
+                        norm_significand <= temp_significand_product(temp_significand_product'high-1 downto 0);
                         norm_exponent <= unnorm_exponent_stage;
                     end if;
                     result <= output_sign_stage & norm_exponent & norm_significand;
-                    end if;
+                end if;
         end process proc_norm;
         o_result <= result;
 end architecture RTL;
